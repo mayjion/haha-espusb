8fe34718abd99dd815465c907f1353d313d8184d
mayjion <461753544@qq.com>
2025-05-30 08:51:08 +0800

    usb cdc serial transport

---
+++ examples/peripherals/usb/device/tusb_composite_msc_serialdevice/main/tusb_composite_main.h
@@ -0,0 +1,9 @@
+#ifndef _TUSB_COMPOSITE_MAIN_H_
+#define _TUSB_COMPOSITE_MAIN_H_
+
+#define MACSTR "%02x:%02x:%02x:%02x:%02x:%02x"
+#define MAC2STR(mac) mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]
+
+
+
+#endif
